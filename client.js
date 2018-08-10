548c2c86461d3f57bdfcfe92dd5bd47421f45d14
May Chen <maychen@Mays-MacBook-Pro.local>
2018-08-09 21:26:39 -1000

    removed http code, reverted back to using net.

--- client.js
+++ client.js
@@ -1,22 +1,9 @@
-// const http = require("http");
+const net = require("net");
 
-// const client = net.createConnection(8080, "0.0.0.0", () => {
-//   client.write("User");
-//   client.on("data", data => {
-//     console.log(data.toString());
-//   });
-//   process.stdin.pipe(client);
-// });
-
-const http = require('http');
-const options = {
-  host: 'www.devleague.com/apply',
-};
-const req = http.get(options);
-req.end();
-req.once('response', (res) => {
-  const ip = '0.0.0.0';
-  const port = 8080;
-  console.log(`Your IP address is ${ip} and your source port is ${port}.`);
-  // consume response object
+const client = net.createConnection(6969, "0.0.0.0", () => {
+  client.write("HELLO IM HERE");
+  client.on("data", data => {
+    console.log(data.toString());
+  });
+  process.stdin.pipe(client);
 });
--- server.js
+++ server.js
@@ -1,27 +1,19 @@
-const http = require("http");
+const net = require("net");
 
-// const server = http.createServer(client => {
-//   //"Connect" listener
-//   console.log("Client connected!");
-//   client.write("hi");
+const server = net.createServer(client => {
+  //"Connect" listener
+  console.log("Client connected!");
+  client.write("hi");
 
-//   client.on("data", data => {
-//     console.log(data.toString());
-//     let msg = data.toString();
-//   });
+  client.on("data", data => {
+    console.log(data.toString());
+    let msg = data.toString();
+  });
 
-// });
-
-const hostname = "0.0.0.0";
-const port = 8080;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'test/plain');
-  res.end('Hello World\n');
-})
-
-
-server.listen(port, hostname, () => {
-  console.log("Server running at http://" + hostname + ':' + port + '/');
 });
+
+server.listen(6969, () => {
+  console.log("Server listening on port 6969");
+});
+
+
