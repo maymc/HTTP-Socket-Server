5e7751d026db37be77617995c1f0b9869b67445f
May Chen <maychen@Mays-MacBook-Pro.local>
2018-08-09 19:42:17 -1000

    Initial commit for server file

---
+++ server.js
@@ -0,0 +1,17 @@
+const net = require("net");
+
+const server = net.createServer(client => {
+
+  //"Connect" listener
+  console.log("Client connected!");
+  client.write("hi");
+
+  client.on("data", data => {
+    console.log(data.toString());
+    let msg = data.toString();
+  })
+
+  server.listen(8080, () => {
+    console.log("Server listening on port 8080");
+  })
+})
