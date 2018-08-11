057bdc8b90f9145b5ef4121b0879217a39e6d27c
May Chen <maychen@Mays-MacBook-Pro.local>
2018-08-10 21:37:10 -1000

    Node is able to take a single arument with host and uri (uniform resource identifier -  resource to deliver to the client). Server is able to respond and display the response message body to the terminal. Node client also displays a help/usage message that explains how to use the client if node client is run with no arguments.

--- client.js
+++ client.js
@@ -1,29 +1,24 @@
 const net = require("net");
 
-// console.log(process.argv[2]);
-// let slashIndex = process.argv[2].indexOf("/");
-// let host = process.argv[2].slice(0, slashIndex);
-// let path = process.argv[2].slice(slashIndex);
+
 const client = net.createConnection(8080, "0.0.0.0", () => {
-  // client.write("GET /index.html HTTP/1.1\nhost: www.devleague.com");
-  //this is inputted host and path from user
 
-  //LOOKS LIKE THIS ON SERVER
-  //GET /index.html HTTP/1.1
-  //host: localhost:8080
-
-  //DO THIS ON CLIENT:
-  //Use process.argv to get the host/path you entered in in terminal (ie: node client.js localhost:8080/blahblahblah)
-  //1. Split the string into host and path and store it in variables
-  //2. concat the host and path
-  //3. then client write it
-
-  // client.write("GET /index.html HTTP/1.1\nhost: www.devleague.com");
-  // client.write("GET " + path + " HTTP/1.1\nhost: " + host);
   client.on("data", data => {
     console.log(data.toString());
-    let clientMsg = data.toString();
-    console.log("clientMsg: ", clientMsg);
+    let msgFromServer = data.toString();
+    console.log("\nmsgFromServer:\n", msgFromServer);
+
+    if (process.argv[2] === undefined) {
+      client.write("\nHelp/Usage:\nTo use this client, please run the client.js file with nodemon and add a host/path.");
+      client.write("\n(ie: nodemon client.js localhost:8080/yourPathName)\n");
+    }
+    else {
+      console.log("\nprocess.argv:\n", process.argv[2]);
+      let slashIndex = process.argv[2].indexOf("/");
+      let host = process.argv[2].slice(0, slashIndex);
+      let path = process.argv[2].slice(slashIndex);
+      client.write("GET " + path + " HTTP/1.1\nhost: " + host);
+    }
   });
   process.stdin.pipe(client);
 });
