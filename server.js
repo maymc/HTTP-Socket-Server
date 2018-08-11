00fd9ff1f72c4435a1448deac44dbbd132eaed5f
Jason <Jason@Colins-MacBook-Pro.local>
2018-08-10 17:52:38 -1000

    added server http response line

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const net = require("net");
 const server = net.createServer(client => {
   //"Connect" listener
   console.log("Client connected!");
-  client.write("hi");
-
+  client.write("HTTP/1.x 200 OK\nServer: Ubuntu Server");
   client.on("data", data => {
     console.log(data.toString());
     let msg = data.toString();
   });
-
+  
 });
-
+console.log('process.argv', process.argv)
 server.listen(8080, () => {
   console.log("Server listening on port 6969");
 });
