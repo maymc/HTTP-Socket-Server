9aeaf319b33857144e2d3cec19cfacc813055a69
May Chen <maychen@Mays-MacBook-Pro.local>
2018-08-13 23:39:34 -1000

    Added new js files for metadata. Got server sort of working but not really MVP yet. Server is able to response to one simple request but nothing that pulls from the other JS files.

--- client.js
+++ client.js
@@ -1,26 +1,30 @@
 const net = require("net");
 
-
 const client = net.createConnection(8080, "0.0.0.0", () => {
 
   client.on("data", data => {
-    console.log(data.toString());
+    //console.log(data.toString());
     let msgFromServer = data.toString();
     console.log("\nmsgFromServer:\n", msgFromServer);
 
+    console.log(process.argv);
     //If node client is run with no arguments then display a help message that explains how to use your client, including all available options
     if (process.argv[2] === undefined) {
       client.write("\nHelp/Usage:\nTo use this client, please run the client.js file with nodemon and add a host/path.");
       client.write("\n(ie: nodemon client.js localhost:8080/yourPathName)\n");
     }
     else {
-      //Else, node command is able to take in a single argument with host and uri to request a resource.
+      //Else, node command is able to take in a single argument with host and URI (Uniform Resource Identifier) to request a resource.
+      //Get the URI and separate the host and path out
       console.log("\nprocess.argv:\n", process.argv[2]);
       let slashIndex = process.argv[2].indexOf("/");
       let host = process.argv[2].slice(0, slashIndex);
       let path = process.argv[2].slice(slashIndex);
+
       //Transmits 'standard' HTTP headers to the server (Header Request Line: GET /index.html HTTP/1.1 ; Header: any host)
-      client.write("GET " + path + " HTTP/1.1\nhost: " + host);
+      let date = new Date();
+
+      client.write("GET " + path + " HTTP/1.1" + "\nDate: " + date + "\nHost: " + host + "\nUser-Agent: DevLeagueClient");
     }
   });
   process.stdin.pipe(client);
--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const net = require("net");
+const module404 = require("./404");
+const heliumModule = require("./helium");
+const hydrogenModule = require("./hydrogen");
+const indexModule = require("./index");
+const stylesModule = require("./css/styles");
+let requestedURI = null;
 
 const server = net.createServer(client => {
   //"Connect" listener
-  console.log("Client connected!");
-  client.write("HTTP/1.x 200 OK\nServer: Ubuntu Server");
+  console.log("Client connected!\n");
+
+  //Process.argv prints out on server side when the client.write above goes through on the client side.
+  // console.log("process.argv:\n", process.argv);
+
   client.on("data", data => {
-    console.log(data.toString());
+    //console.log(data.toString());
     let msgFromClient = data.toString();
+    console.log("\nmsgFromClient:\n", msgFromClient);
+    let msgElements = msgFromClient.split(" ");
+
+    //console.log(msgFromClient.split(" "));
+    requestedURI = msgElements[1];
+    console.log("Requested URI: ", requestedURI);
   });
+
+  //Transmit 'standard' HTTP Headers to the client. (So this is the response to client's request). HTTP Response is the server replying to the client's original HTTP Request with a message and the requested resource.
+  //HTTP Response includes a response message which is made with : A status line and response headers
+
+  //If path is not found in your routes, return 404 status code and output some text/html content
+  let date = new Date();
+
+  if (requestedURI === "/") {
+    client.write("HTTP/1.0 404 Not Found\nDate: " + date + "\nServer: DevLeagueServer\n\n");
+  }
+  else {
+    client.write("HTTP/1.0 404 Not Found\nDate: " + date + "\nServer: DevLeagueServer\n\n");
+  }
 
 });
 
+//Server listens on port 8080
 server.listen(8080, () => {
   console.log("Server listening on port 6969");
 });
