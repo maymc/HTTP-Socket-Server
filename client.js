0ad407fe417e7d448df576f4617285642e0a0689
May Chen <maychen@Mays-MacBook-Pro.local>
2018-08-16 00:14:35 -1000

    Client is working with server. Moved the client.on function to be after what the client writes because client.on gets the server's response so the client needs to make the request first before a response can be returned.

--- client.js
+++ client.js
@@ -1,30 +1,32 @@
 const net = require("net");
 
-const client = net.createConnection(8080, "0.0.0.0", () => {
+console.log("\nprocess.argv:", process.argv);
 
+let host = process.argv[2];
+console.log("host:", process.argv[2]);
+let port = process.argv[3];
+console.log("port:", process.argv[3]);
+let path = process.argv[4];
+console.log("path:", process.argv[4]);
+
+const client = net.createConnection(port, host, () => {
+  let date = new Date();
+
+  //If node client is run with no arguments then display a help message that explains how to use your client, including all available options
+  if (process.argv[2] === undefined) {
+    client.write("\nHelp/Usage:\nTo use this client, please run the client.js file with nodemon and add a host/path.");
+    client.write("\n(ie: nodemon client.js localhost:8080/yourPathName)\n");
+  }
+  //Else, node command is able to take in a single argument with host and URI (Uniform Resource Identifier) to request a resource.
+  else {
+    client.write("GET " + path + " HTTP/1.1" + "\nDate: " + date + "\nHost: " + host + "\nUser-Agent: DevLeagueClient");
+  }
+
+  //This is after because this is the server's response
   client.on("data", data => {
     let msgFromServer = data.toString();
     console.log("\nmsgFromServer:\n", msgFromServer);
-
-    //console.log(process.argv);
-    //If node client is run with no arguments then display a help message that explains how to use your client, including all available options
-    if (process.argv[2] === undefined) {
-      client.write("\nHelp/Usage:\nTo use this client, please run the client.js file with nodemon and add a host/path.");
-      client.write("\n(ie: nodemon client.js localhost:8080/yourPathName)\n");
-    }
-    else {
-      //Else, node command is able to take in a single argument with host and URI (Uniform Resource Identifier) to request a resource.
-      //Get the URI and separate the host and path out
-      console.log("\nprocess.argv[2]:", process.argv[2]);
-      let slashIndex = process.argv[2].indexOf("/");
-      let host = process.argv[2].slice(0, slashIndex);
-      let path = process.argv[2].slice(slashIndex);
-
-      //Transmits 'standard' HTTP headers to the server (Header Request Line: GET /index.html HTTP/1.1 ; Header: any host)
-      let date = new Date();
-
-      client.write("GET " + path + " HTTP/1.1" + "\nDate: " + date + "\nHost: " + host + "\nUser-Agent: DevLeagueClient");
-    }
   });
   process.stdin.pipe(client);
+
 });
